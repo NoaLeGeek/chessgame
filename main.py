8c8c3014824caa751aa7f60d4b345bb7e28d9718
NoaLeGeek <noacraft68@gmail.com>
2023-09-15 08:05:38 +0200

    Added get_position

--- main.py
+++ main.py
@@ -1,42 +1,55 @@
 import pygame
 import os
 
+clock = pygame.time.Clock()
+width, height = 760, 760
+window = pygame.display.set_mode((width, height))
+rows, cols = 8, 8
+square_size = width // cols
+tile_assets = {"brown": ((237, 214, 176), (184, 135, 98)),
+               "green": ((233, 237, 204), (119, 153, 84)),
+               "sky": ((240, 241, 240), (196, 216, 228)),
+               "8-bit": ((243, 243, 244), (106, 155, 65)),
+               "purple": ((240, 241, 240), (132, 118, 186)),
+               "blue": ((234, 233, 210), (75, 115, 153)),
+               "bubblegum": ((254, 255, 254), (251, 217, 225)),
+               "checkers": ((199, 76, 81), (48, 48, 48)),
+               "light": ((216, 217, 216), (168, 169, 168)),
+               "orange": ((250, 228, 174), (209, 136, 21)),
+               "red": ((245, 219, 195), (187, 87, 70)),
+               "tan": ((237, 203, 165), (216, 164, 109))}
+selected_asset = "brown"
+
+
+def get_position(x, y):
+    return x // square_size, y // square_size
+
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
     run = True
     fps = 60
-    width, height = 760, 760
-    win = pygame.display.set_mode((width, height))
-    rows, cols = 8, 8
-    square_size = width // cols
-    tile_assets = {"brown": ((237, 214, 176), (184, 135, 98)),
-                   "green": ((233, 237, 204), (119, 153, 84)),
-                   "sky": ((240, 241, 240), (196, 216, 228)),
-                   "8-bit": ((243, 243, 244), (106, 155, 65)),
-                   "purple": ((240, 241, 240), (132, 118, 186)),
-                   "blue": ((234, 233, 210), (75, 115, 153)),
-                   "bubblegum": ((254, 255, 254), (251, 217, 225)),
-                   "checkers": ((199, 76, 81), (48, 48, 48)),
-                   "light": ((216, 217, 216), (168, 169, 168)),
-                   "orange": ((250, 228, 174), (209, 136, 21)),
-                   "red": ((245, 219, 195), (187, 87, 70)),
-                   "tan": ((237, 203, 165), (216, 164, 109))}
-    selected_asset = "brown"
     # Piece images are stored in the following order: pawn, knight, bishop, rook, queen, king. White pieces come first.
     pieces = {}
     for piece in ["wP", "wN", "wB", "wR", "wQ", "wK", "bP", "bN", "bB", "bR", "bQ", "bK"]:
         pieces[piece] = pygame.transform.scale(pygame.image.load(
-            os.path.join("assets", ("white" if piece.startswith("w") else "black") + "Pieces", selected_asset, piece + ".png")), (square_size, square_size))
+            os.path.join("assets", ("white" if piece.startswith("w") else "black") + "Pieces", selected_asset,
+                         piece + ".png")), (square_size, square_size))
     while run:
         clock.tick(fps)
-        win.blit(pieces["wR"], (50, 50))
-        pygame.display.update()
+        window.blit(pieces["wR"], (50, 50))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    pass
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed()[0]:
+                    location = pygame.mouse.get_pos()
+                    window.blit(pieces["wR"], (location[0], location[1]))
+        pygame.display.update()
 
 
 if __name__ == "__main__":
