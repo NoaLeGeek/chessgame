e3c1c5d047a02ffadf838cf037f78f08b7ad2836
NoaLeGeek <62206118+NoaLeGeek@users.noreply.github.com>
2024-01-15 13:36:42 +0100

    added offset for promotion

--- Board.py
+++ Board.py
@@ -15,10 +15,10 @@ class Board:
         self.debug = False
 
     def draw_board(self):
-        self.frame.fill(tile_assets[selected_tile][0])
+        self.frame.fill(tile_assets[selected_tile][1])
         for row in range(rows):
             for column in range(row % 2, columns, 2):
-                pygame.draw.rect(self.frame, tile_assets[selected_tile][1],(row * square_size, column * square_size, square_size, square_size))
+                pygame.draw.rect(self.frame, tile_assets[selected_tile][0],(row * square_size, column * square_size, square_size, square_size))
 
     def draw_piece(self, piece, window):
         window.blit(piece.image, (piece.x, piece.y))
@@ -32,18 +32,19 @@ class Board:
                 if self.debug:
                     self.frame.blit(pygame.font.SysFont("monospace", 15).render(f"({column},{row})", 1, (0, 0, 0)), (row*square_size+35, column*square_size+60))
                 piece = self.board[row][column]
-                if piece != 0:
-                    self.draw_piece(piece, self.frame)
+                if piece == 0 or (isinstance(piece, Pawn) and piece.promotion[0]):
+                    continue
+                self.draw_piece(piece, self.frame)
 
     def draw_moves(self, moves):
         for pos in moves:
             row, column = pos[0], pos[1]
             pygame.draw.circle(self.frame, (127, 255, 0), (column * square_size + square_size // 2, row * square_size + square_size // 2), square_size // 8)
 
-    def draw_promotion(self, promotion):
+    def draw_promotion(self, promotion, offset):
         # TODO promotion is offbeat by 1 square if it's a capture
         for row in range((0 if promotion.color == 1 else 7), (0 if promotion.color == 1 else 7)+4*promotion.color, promotion.color):
-            pygame.draw.rect(self.frame, (255, 255, 255, 0.5), (promotion.column * square_size, row * square_size, square_size, square_size))
+            pygame.draw.rect(self.frame, (255, 255, 255, 0.5), ((promotion.column + offset) * square_size, row * square_size, square_size, square_size))
 
     def draw_test(self):
         for row in range(self.rows):
