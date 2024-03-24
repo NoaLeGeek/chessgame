b98b8d9b7c4117fd9e0e368ab2ffc892d71b5961
NoaLeGeek <noacraft68@gmail.com>
2024-03-24 20:02:15 +0100

    Updating steganographie

--- steganographie.py
+++ steganographie.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import tkinter as tk
 import ctypes
+import os
 from tkinter import filedialog
 
 width, height = ctypes.windll.user32.GetSystemMetrics(0), ctypes.windll.user32.GetSystemMetrics(1) - 48 - 23
@@ -12,18 +13,21 @@ first_image = ""
 second_image = ""
 win = tk.Tk()
 
-def select_file_toOpen(text, index):
+def select_file(text, index):
     fileTypes = (('PNG files', '*.png'),)
     fileName = filedialog.askopenfilename(title=text, initialdir='~', filetypes=fileTypes)
     if index == 1:
         first_image = fileName
-        first_image_label['text'] = f"First image selected: {fileName}"
-        first_image_image = tk.Image()
+        if first_image == "":
+            return
+        first_image_label['text'] = f"First image selected:\n{os.path.basename(fileName)}"
     else:
         second_image = fileName
-        second_image_label['text'] = f"Second image selected: {fileName}"
+        if second_image == "":
+            return
+        second_image_label['text'] = f"Second image selected:\n{os.path.basename(fileName)}"
 
-def select_file_toSave(text):
+def save_file(text):
     fileTypes = (('PNG files', '*.png'),)
     fileName = filedialog.asksaveasfilename(title=text, initialdir='~', filetypes=fileTypes)
     return fileName
@@ -44,36 +48,46 @@ def toggle_buttons():
     mode *= -1
 
 def flip_arrow():
+    global arrow
     arrow *= -1
+    arrow_button['text'] = ("<" if arrow == 1 else "") + "-"*10 + (">" if arrow == -1 else "")
 
 win.geometry(f"{width}x{height}")
 win.title("title")
-win.config(bg="deepskyblue")
-for i in range(6):
-    win.columnconfigure(i, weight=1)
-win.rowconfigure(3, weight=1)
+win.config(bg="white")
+win.columnconfigure(tuple(range(6)), weight=1)
+win.rowconfigure(tuple(range(6)), weight=1)
+
 hide_button = tk.Button(win, text="Hide", command=toggle_buttons)
 hide_button.bindtags(("hide",) + hide_button.bindtags())
-hide_button.grid(row=0, column=0, columnspan=3, sticky="ew")
+hide_button.grid(row=0, column=0, columnspan=3, sticky="new")
 hide_button.config(state=tk.DISABLED)
-first_image_button = tk.Button(win, text="Select first image", command=lambda: select_file_toOpen("Select first image", 1))
+
+first_image_button = tk.Button(win, text="Select first image", command=lambda: select_file("Select first image", 1))
 first_image_button.bindtags(("hide.first_button",) + first_image_button.bindtags())
-first_image_button.grid(row=1, column=0, columnspan=2, pady=50)
-first_image_label = tk.Label(win, text="No first image selected")
+first_image_button.grid(row=1, column=0, columnspan=2, sticky="s")
+
+first_image_label = tk.Label(win, text="No first image selected", justify=tk.CENTER)
 first_image_label.bindtags(("hide.first_label",) + first_image_label.bindtags())
-first_image_label.grid(row=2, column=0, columnspan=2)
-arrow_button = tk.Button(win, text="🡰" + "-"*10, command=flip_arrow) #probably do an image 
+first_image_label.grid(row=2, column=0, columnspan=2, sticky="n")
+
+arrow_button = tk.Button(win, text="<" + "-"*10, command=flip_arrow)
 arrow_button.bindtags(("arrow",) + first_image_button.bindtags())
-arrow_button.grid(row=1, column=2, columnspan=2, pady=50, sticky="ew")
-second_image_button = tk.Button(win, text="Select second image", command=lambda: select_file_toOpen("Select second image", -1))
+arrow_button.grid(row=1, column=2, columnspan=2, sticky="sew")
+
+second_image_button = tk.Button(win, text="Select second image", command=lambda: select_file("Select second image", -1))
 second_image_button.bindtags(("hide.second_button",) + second_image_button.bindtags())
-second_image_button.grid(row=1, column=4, columnspan=2, pady=50)
-second_image_label = tk.Label(win, text="No second image selected")
+second_image_button.grid(row=1, column=4, columnspan=2, sticky="s")
+
+second_image_label = tk.Label(win, text="No second image selected", justify=tk.CENTER)
 second_image_label.bindtags(("hide.second_label",) + second_image_label.bindtags())
-second_image_label.grid(row=2, column=4, columnspan=2)
+second_image_label.grid(row=2, column=4, columnspan=2, sticky="n")
+
 show_button = tk.Button(win, text="Show", command=toggle_buttons)
 show_button.bindtags(("show",) + show_button.bindtags())
-show_button.grid(row=0, column=3, columnspan=3, sticky="ew")
+show_button.grid(row=0, column=3, columnspan=3, sticky="new")
+
 quit_button = tk.Button(win, text="Quit", command=win.destroy)
-quit_button.grid(row=4, column=0, columnspan=6, sticky="ew")
+quit_button.grid(row=5, column=0, columnspan=6, sticky="sew")
+
 win.mainloop()
