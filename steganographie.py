9a1803853c0a5bf26223b852b4a715137a27cb5c
NoaLeGeek <noacraft68@gmail.com>
2024-03-31 15:19:10 +0200

    Finished steganographie.py Changed the variables names

--- steganographie.py
+++ steganographie.py
@@ -24,35 +24,36 @@ def select_file(text, index):
     global first_image, second_image, show_image
     fileTypes = (('PNG files', '*.png'),)
     filePath = filedialog.askopenfilename(title=text, initialdir='~', filetypes=fileTypes)
-    if index == 1:
-        first_image = filePath
-        if first_image == "":
-            return
-        first_label.bindtags(("hide.first_label.selected",) + first_label.bindtags()[1:])
-        update_widget(first_label)
-        first_label['text'] = first_label['text'].format(os.path.basename(first_image))
-        show_first_button.config(state=tk.NORMAL)
-    if index == 2:
-        show_image = filePath
-        if show_image == "":
-            return
-        select_label.bindtags(("show.select_label.selected",) + select_label.bindtags()[1:])
-        update_widget(select_label)
-        select_label['text'] = select_label['text'].format(os.path.basename(show_image))
-        show_initial_button.config(state=tk.NORMAL)
-        show_reveal_button.config(state=tk.NORMAL)
-        reveal_button.config(state=tk.NORMAL)
-        initial_button.config(state=tk.NORMAL)
-    else:
-        second_image = filePath
-        if second_image == "":
-            return
-        second_label.bindtags(("hide.second_label.selected",) + second_label.bindtags()[1:])
-        update_widget(second_label)
-        second_label['text'] = second_label['text'].format(os.path.basename(second_image))
-        show_second_button.config(state=tk.NORMAL)
+    match index:
+        case 1:
+            first_image = filePath
+            if first_image == "":
+                return
+            h_first_label.bindtags(("hide.h_first_label.selected",) + h_first_label.bindtags()[1:])
+            update_widget(h_first_label)
+            h_show_first_button.config(state=tk.NORMAL)
+        case 2:
+            second_image = filePath
+            if second_image == "":
+                return
+            h_second_label.bindtags(("hide.h_second_label.selected",) + h_second_label.bindtags()[1:])
+            update_widget(h_second_label)
+            h_show_second_button.config(state=tk.NORMAL)
+        case 3:
+            show_image = filePath
+            if show_image == "":
+                return
+            select_label.bindtags(("show.select_label.selected",) + select_label.bindtags()[1:])
+            update_widget(select_label)
+            show_initial_button.config(state=tk.NORMAL)
+            show_reveal_button.config(state=tk.NORMAL)
+            reveal_button.config(state=tk.NORMAL)
+            initial_button.config(state=tk.NORMAL)
+            save_initial_button.config(state=tk.NORMAL)
+            save_reveal_button.config(state=tk.NORMAL)
+            show_select_button.config(state=tk.NORMAL)
     if first_image != "" and second_image != "":
-        hide_image_button.config(state=tk.NORMAL)
+        h_image_button.config(state=tk.NORMAL)
 
 
 def save_file(text):
@@ -72,9 +73,7 @@ def open_image(index):
             case 2:
                 image = Image.open(second_image)
             case 3:
-                image = Image.open(initial_image)
-            case 4:
-                image = Image.open(revealed_image)
+                image = Image.open(show_image)
         return image
     except FileNotFoundError:
         messagebox.showerror(translate("error.error"), translate("error.file_not_found"))
@@ -84,7 +83,7 @@ def open_image(index):
 def change_mode():
     global mode
     mode *= -1
-    for widget in [hide_button, show_button]:
+    for widget in [h_mode_button, show_button]:
         widget.config(state=tk.NORMAL if widget['state'] == tk.DISABLED else tk.DISABLED)
     change_widgets(win, "hide" if mode == 1 else "show")
         
@@ -123,8 +122,8 @@ def get_hide_image():
                 hidden_image.putpixel((i, j), (r, g, b))
             else:
                 hidden_image.putpixel((i, j), pixels1[i, j])
-    save_hidden_image.config(state=tk.NORMAL)
-    show_hidden_image.config(state=tk.NORMAL)
+    h_save_button.config(state=tk.NORMAL)
+    h_show_button.config(state=tk.NORMAL)
 
 def get_initial_image():
     global initial_image, show_image
@@ -157,7 +156,7 @@ def get_reveal_image():
 def flip_arrow():
     global arrow
     arrow *= -1
-    arrow_button['text'] = ("<" if arrow == 1 else "") + "-"*10 + (">" if arrow == -1 else "")
+    h_arrow_button['text'] = ("<" if arrow == 1 else "") + "-"*10 + (">" if arrow == -1 else "")
 
 def translate(key):
     global language
@@ -174,10 +173,13 @@ def update_widget(widget):
             widget.config(text=translate(widget.bindtags()[0]))
         if isinstance(widget, tk.Tk):
             widget.title(translate("title"))
-        if widget.bindtags()[0] == "hide.first_label.selected":
-            widget['text'] = translate(widget.bindtags()[0]).format(os.path.basename(first_image))
-        if widget.bindtags()[0] == "hide.second_label.selected":
-            widget['text'] = translate(widget.bindtags()[0]).format(os.path.basename(second_image))
+        match widget.bindtags()[0]:
+            case "hide.h_first_label.selected":
+                widget['text'] = translate(widget.bindtags()[0]).format(os.path.basename(first_image))
+            case "hide.h_second_label.selected":
+                widget['text'] = translate(widget.bindtags()[0]).format(os.path.basename(second_image))
+            case "show.select_label.selected":
+                widget['text'] = translate(widget.bindtags()[0]).format(os.path.basename(show_image))
     # On met à jour les widgets enfants du widget.
     if widget.winfo_children():
         for child in widget.winfo_children():
@@ -189,73 +191,73 @@ win.config(bg="white")
 win.columnconfigure(tuple(range(6)), weight=1)
 win.rowconfigure(tuple(range(6)), weight=1)
 
-hide_button = tk.Button(win, command=change_mode)
-hide_button.bindtags(("button.hide",) + hide_button.bindtags())
-hide_button.grid(row=0, column=0, columnspan=3, sticky="new")
-hide_button.config(state=tk.DISABLED)
+h_mode_button = tk.Button(win, command=change_mode)
+h_mode_button.bindtags(("button.hide",) + h_mode_button.bindtags())
+h_mode_button.grid(row=0, column=0, columnspan=3, sticky="new")
+h_mode_button.config(state=tk.DISABLED)
 
-first_button = tk.Button(win, command=lambda: select_file(translate("hide.first_button"), 1), width=round(WIDTH/3))
-first_button.bindtags(("hide.first_button",) + first_button.bindtags())
-first_button.grid(row=0, column=0, columnspan=2, sticky="s")
+h_first_button = tk.Button(win, command=lambda: select_file(translate("hide.h_first_button"), 1), width=round(WIDTH/3))
+h_first_button.bindtags(("hide.h_first_button",) + h_first_button.bindtags())
+h_first_button.grid(row=0, column=0, columnspan=2, sticky="s")
 
-first_label = tk.Label(win, justify=tk.CENTER, width=round(WIDTH/3))
-first_label.bindtags(("hide.first_label.not_selected",) + first_label.bindtags())
-first_label.grid(row=1, column=0, columnspan=2, sticky="n")
+h_first_label = tk.Label(win, justify=tk.CENTER, width=round(WIDTH/3))
+h_first_label.bindtags(("hide.h_first_label.not_selected",) + h_first_label.bindtags())
+h_first_label.grid(row=1, column=0, columnspan=2, sticky="n")
 
-show_first_button = tk.Button(win, command=lambda: open_image(1).show(), width=round(WIDTH/3))
-show_first_button.bindtags(("hide.show_first_button",) + show_first_button.bindtags())
-show_first_button.grid(row=1, column=0, columnspan=2, sticky="s")
-show_first_button.config(state=tk.DISABLED)
+h_show_first_button = tk.Button(win, command=lambda: open_image(1).show(), width=round(WIDTH/3))
+h_show_first_button.bindtags(("hide.h_show_first_button",) + h_show_first_button.bindtags())
+h_show_first_button.grid(row=1, column=0, columnspan=2, sticky="s")
+h_show_first_button.config(state=tk.DISABLED)
 
-arrow_button = tk.Button(win, text="<" + "-"*10, command=flip_arrow, width=round(WIDTH/3))
-arrow_button.bindtags(("hide.noTranslation",) + arrow_button.bindtags())
-arrow_button.grid(row=0, column=2, columnspan=2, sticky="sew")
+h_arrow_button = tk.Button(win, text="<" + "-"*10, command=flip_arrow, width=round(WIDTH/3))
+h_arrow_button.bindtags(("hide.noTranslation",) + h_arrow_button.bindtags())
+h_arrow_button.grid(row=0, column=2, columnspan=2, sticky="sew")
 
-hide_image_button = tk.Button(win, command=lambda: get_hide_image(), width=round(WIDTH/3))
-hide_image_button.bindtags(("hide.hide_image_button",) + hide_image_button.bindtags())
-hide_image_button.grid(row=1, column=2, columnspan=2, sticky="sew")
-hide_image_button.config(state=tk.DISABLED)
+h_image_button = tk.Button(win, command=lambda: get_hide_image(), width=round(WIDTH/3))
+h_image_button.bindtags(("hide.h_image_button",) + h_image_button.bindtags())
+h_image_button.grid(row=1, column=2, columnspan=2, sticky="sew")
+h_image_button.config(state=tk.DISABLED)
 
-save_hidden_image = tk.Button(win, command=lambda: hidden_image.save(save_file("Save the hidden image"), "PNG"), width=round(WIDTH/3))
-save_hidden_image.bindtags(("hide.save_hidden_image",) + save_hidden_image.bindtags())
-save_hidden_image.grid(row=2, column=2, columnspan=2, sticky="new")
-save_hidden_image.config(state=tk.DISABLED)
+h_save_button = tk.Button(win, command=lambda: hidden_image.save(save_file(translate("hide.h_save_button")), "PNG"), width=round(WIDTH/3))
+h_save_button.bindtags(("hide.h_save_button",) + h_save_button.bindtags())
+h_save_button.grid(row=2, column=2, columnspan=2, sticky="new")
+h_save_button.config(state=tk.DISABLED)
 
-show_hidden_image = tk.Button(win, command=lambda: hidden_image.show(), width=round(WIDTH/3))
-show_hidden_image.bindtags(("hide.show_hidden_image",) + show_hidden_image.bindtags())
-show_hidden_image.grid(row=2, column=2, columnspan=2, sticky="sew")
-show_hidden_image.config(state=tk.DISABLED)
+h_show_button = tk.Button(win, command=lambda: hidden_image.show(), width=round(WIDTH/3))
+h_show_button.bindtags(("hide.h_show_button",) + h_show_button.bindtags())
+h_show_button.grid(row=2, column=2, columnspan=2, sticky="sew")
+h_show_button.config(state=tk.DISABLED)
 
-hide_checkbutton = tk.Checkbutton(win, variable=config_hide, onvalue=True, offvalue=False)
-hide_checkbutton.bindtags(("hide.hide_checkbutton",) + hide_checkbutton.bindtags())
-hide_checkbutton.grid(row=3, column=2, columnspan=2, sticky="n")
+h_checkbutton = tk.Checkbutton(win, variable=config_hide, onvalue=True, offvalue=False)
+h_checkbutton.bindtags(("hide.h_checkbutton",) + h_checkbutton.bindtags())
+h_checkbutton.grid(row=3, column=2, columnspan=2, sticky="n")
 config_hide.set(True)
 
-language_text = tk.Label(win, font=("Helvetica", 9))
-language_text.bindtags(("config.language_text",) + language_text.bindtags())
-language_text.grid(row=3, column=2, columnspan=2, sticky="s")
+language_label = tk.Label(win, font=("Helvetica", 9))
+language_label.bindtags(("config.language_label",) + language_label.bindtags())
+language_label.grid(row=3, column=2, columnspan=2, sticky="s")
 
 language_option = ttk.OptionMenu(win, language, "fr", "fr", "en", command=lambda _: update_widget(win))
 language_option.grid(row=4, column=2, columnspan=2, sticky="n")
 
-second_button = tk.Button(win, command=lambda: select_file(translate("hide.second_button"), -1), width=round(WIDTH/3))
-second_button.bindtags(("hide.second_button",) + second_button.bindtags())
-second_button.grid(row=0, column=4, columnspan=2, sticky="s")
+h_second_button = tk.Button(win, command=lambda: select_file(translate("hide.h_second_button"), 2), width=round(WIDTH/3))
+h_second_button.bindtags(("hide.h_second_button",) + h_second_button.bindtags())
+h_second_button.grid(row=0, column=4, columnspan=2, sticky="s")
 
-second_label = tk.Label(win, justify=tk.CENTER, width=round(WIDTH/3))
-second_label.bindtags(("hide.second_label.not_selected",) + second_label.bindtags())
-second_label.grid(row=1, column=4, columnspan=2, sticky="n")
+h_second_label = tk.Label(win, justify=tk.CENTER, width=round(WIDTH/3))
+h_second_label.bindtags(("hide.h_second_label.not_selected",) + h_second_label.bindtags())
+h_second_label.grid(row=1, column=4, columnspan=2, sticky="n")
 
-show_second_button = tk.Button(win, command=lambda: open_image(2).show(), width=round(WIDTH/3))
-show_second_button.bindtags(("hide.show_second_button",) + show_second_button.bindtags())
-show_second_button.grid(row=1, column=4, columnspan=2, sticky="s")
-show_second_button.config(state=tk.DISABLED)
+h_show_second_button = tk.Button(win, command=lambda: open_image(2).show(), width=round(WIDTH/3))
+h_show_second_button.bindtags(("hide.h_show_second_button",) + h_show_second_button.bindtags())
+h_show_second_button.grid(row=1, column=4, columnspan=2, sticky="s")
+h_show_second_button.config(state=tk.DISABLED)
 
 show_button = tk.Button(win, command=change_mode)
 show_button.bindtags(("button.show",) + show_button.bindtags())
 show_button.grid(row=0, column=3, columnspan=3, sticky="new")
 
-select_button = tk.Button(win, command=lambda: select_file(translate("show.select_button"), 2))
+select_button = tk.Button(win, command=lambda: select_file(translate("show.select_button"), 3))
 select_button.bindtags(("show.select_button",) + select_button.bindtags())
 select_button.grid(row=0, column=2, columnspan=2, sticky="sew")
 select_button.grid_remove()
@@ -265,29 +267,47 @@ select_label.bindtags(("show.select_label.not_selected",) + select_label.bindtag
 select_label.grid(row=1, column=2, columnspan=2, sticky="new")
 select_label.grid_remove()
 
-initial_button = tk.Button(win, command=initial_image)
+show_select_button = tk.Button(win, command=lambda: open_image(3).show())
+show_select_button.bindtags(("show.show_select_button",) + show_select_button.bindtags())
+show_select_button.grid(row=1, column=2, columnspan=2, sticky="sew")
+show_select_button.grid_remove()
+show_select_button.config(state=tk.DISABLED)
+
+initial_button = tk.Button(win, command=get_initial_image)
 initial_button.bindtags(("show.initial_button",) + initial_button.bindtags())
-initial_button.grid(row=1, column=1, columnspan=2, sticky="sew")
+initial_button.grid(row=2, column=1, columnspan=2, sticky="new")
 initial_button.grid_remove()
 initial_button.config(state=tk.DISABLED)
 
-show_initial_button = tk.Button(win, command=lambda: open_image(3).show())
+show_initial_button = tk.Button(win, command=lambda: initial_image.show())
 show_initial_button.bindtags(("show.show_initial_button",) + show_initial_button.bindtags())
-show_initial_button.grid(row=2, column=1, columnspan=2, sticky="new")
+show_initial_button.grid(row=2, column=1, columnspan=2, sticky="sew")
 show_initial_button.grid_remove()
 show_initial_button.config(state=tk.DISABLED)
 
-reveal_button = tk.Button(win, command=revealed_image)
+save_initial_button = tk.Button(win, command=lambda: initial_image.save(save_file(translate("show.save_initial_image")), "PNG"))
+save_initial_button.bindtags(("show.save_initial_button",) + save_initial_button.bindtags())
+save_initial_button.grid(row=3, column=1, columnspan=2, sticky="new")
+save_initial_button.grid_remove()
+save_initial_button.config(state=tk.DISABLED)
+
+reveal_button = tk.Button(win, command=get_reveal_image)
 reveal_button.bindtags(("show.reveal_button",) + reveal_button.bindtags())
-reveal_button.grid(row=1, column=3, columnspan=2, sticky="sew")
+reveal_button.grid(row=2, column=3, columnspan=2, sticky="new")
 reveal_button.grid_remove()
 reveal_button.config(state=tk.DISABLED)
 
-show_reveal_button = tk.Button(win, command=lambda: open_image(4).show())
+show_reveal_button = tk.Button(win, command=lambda: revealed_image.show())
 show_reveal_button.bindtags(("show.show_reveal_button",) + show_reveal_button.bindtags())
-show_reveal_button.grid(row=2, column=3, columnspan=2, sticky="new")
+show_reveal_button.grid(row=2, column=3, columnspan=2, sticky="sew")
 show_reveal_button.grid_remove()
 show_reveal_button.config(state=tk.DISABLED)
+
+save_reveal_button = tk.Button(win, command=lambda: revealed_image.save(save_file(translate("show.save_reveal_image")), "PNG"))
+save_reveal_button.bindtags(("show.save_reveal_button",) + save_reveal_button.bindtags())
+save_reveal_button.grid(row=3, column=3, columnspan=2, sticky="new")
+save_reveal_button.grid_remove()
+save_reveal_button.config(state=tk.DISABLED)
 
 quit_button = tk.Button(win, text="Quit", command=win.destroy)
 quit_button.bindtags(("button.quit",) + quit_button.bindtags())
