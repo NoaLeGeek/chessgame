373a18ab476dd5def0bde9b55d18b13e0ee3c632
NoaLeGeek <noacraft68@gmail.com>
2024-04-28 21:55:35 +0200

    Trying to change castling

--- Pieces.py
+++ Pieces.py
@@ -304,9 +304,10 @@ class King(Piece):
         # Castling
         if self.column == (7 + flipped) // 2 and self.first_move:
             # O-O-O
-            if isinstance(board[row][7 * (1 - flipped) // 2], Rook) and board[row][7 * (1 - flipped) // 2].first_move and all(board[row][i] == 0 for i in range(column - flipped, 7 * (1 - flipped) // 2, flipped)):
+            
+            if any([isinstance(board[row][i], Rook) and board[row][i].first_move for i in range(column, 7 * (1 - flipped) - flipped, -flipped)]) and all(board[row][i] == 0 for i in range(column - flipped, 7 * (1 - flipped) // 2, -flipped)):
                 self.available_moves.append((row, (7 + 3 * flipped) // 2))
             # O-O
-            if isinstance(board[row][7 * (1 + flipped) // 2], Rook) and board[row][7 * (1 + flipped) // 2].first_move and all(board[row][i] == 0 for i in range(column + flipped, 7 * (1 + flipped) // 2, flipped)):
+            if any([isinstance(board[row][i], Rook) and board[row][i].first_move for i in range(column, 7 * (1 + flipped) + flipped, flipped)]) and all(board[row][i] == 0 for i in range(column + flipped, 7 * (1 + flipped) // 2, flipped)):
                 self.available_moves.append((row, (7 - 5 * flipped) // 2))
         return self.available_moves
