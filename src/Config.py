f6aabdd4b07e8f6d7a0201e7baf535b1c20b5362
NoaLeGeek <noacraft68@gmail.com>
2024-06-01 19:38:53 +0200

    Added rules config

--- src/Config.py
+++ src/Config.py
@@ -3,18 +3,16 @@ import ctypes
 
 class Config:
     def __init__(self):
-        self.screen_width, self.screen_height = ctypes.windll.user32
+        self.screen_width, self.screen_height = ctypes.windll.user32.GetSystemMetrics(0), ctypes.windll.user32.GetSystemMetrics(1)
         self.config = configparser.ConfigParser()
         self.config.read('config.cfg')
-        self.height = self.config.getint('GENERAL', 'height') if self.config.getint('GENERAL', 'height') else self.generate_height()
-        self.width = int(self.height * 0.85)
+        self.height = self.config.getint('GENERAL', 'height') if self.config.getint('GENERAL', 'height') else self.screen_height
+        self.width = self.height
+        self.rules = []
         self.tile_size = self.height//12
         self.margin = self.tile_size//2
         self.fps = self.config.getint('GENERAL', 'fps')
         self.piece_asset = self.config.get('ASSETS', 'piece')
-
-    def generate_height(self):
-        return round((self.user32.GetSystemMetrics(1)-100)/12)*12
 
     def save(self):
         pass
