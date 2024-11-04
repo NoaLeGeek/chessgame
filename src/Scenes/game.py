80f068bf8ea8afe6cdb74308b23eb430a99ff4dc
NoaLeGeek <noacraft68@gmail.com>
2024-11-04 17:36:09 +0100

    Transferring code to laptop

--- src/Scenes/game.py
+++ src/Scenes/game.py
@@ -26,6 +26,7 @@ class Game(Scene):
 
     def draw_objects(self, screen):
         for tile in self.board.board.values():
+            assert tile is not None, "Tile is None"
             if self.board.debug:
                 screen.blit(pygame.font.SysFont("monospace", 15).render(f"({tile.column},{tile.row})", 1, (0, 0, 0)), (tile.row*self.config.tile_size+35, tile.column*self.config.tile_size+60))
             if self.config.piece_asset != "blindfold":
